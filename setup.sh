1dfb0eb9d5f4d13ea7da1f44efead7eac988c5d6
xkhs <70058702+xkhs@users.noreply.github.com>
2020-09-20 01:50:26 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,4 +20,4 @@ wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 
 # Tracker
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
-echo "bt-tracker=$tracker_list" >> aria2c.conf
+echo "\nbt-tracker=$tracker_list" >> aria2c.conf
