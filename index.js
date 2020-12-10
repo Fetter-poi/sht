c349deb9f513c9b3723e6f27546d79cbe5206938
xkhs <70058702+xkhs@users.noreply.github.com>
2020-12-10 23:28:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -155,14 +155,14 @@ async function restore_undownloaded(){
 }
 
 async function updateDB(){
-    let respone = await gotInstance.get('forum.php?mod=forumdisplay&fid=140&page=1');
+    let respone = await gotInstance.get('forum.php?mod=forumdisplay&fid=148&page=1');
     const html = respone.body;
     const $ = cheerio.load(html);
     // console.log($("tbody[id^='normalthread']").length);
     const lastPageHref = $("div.pg > a.last").attr('href');
     response = await gotInstance.get(lastPageHref);
     console.log(lastPageHref);
-    const regex = /(forum-140-)(\d*).html/;
+    const regex = /(forum-148-)(\d*).html/;
     const match = lastPageHref.match(regex);
     const forumPrefix = match[1];
     const maxPageNumber = parseInt(match[2]);
