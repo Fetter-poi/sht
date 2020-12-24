34e6721db3bd6c3b862e3f92dfc80b5e7addedfb
xkhs <70058702+xkhs@users.noreply.github.com>
2020-12-25 00:25:04 +0800

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -5,7 +5,7 @@
 
 # 限制最低上传大小，仅 BT 多文件下载时有效，用于过滤无用文件。低于此大小的文件将被删除，不会上传。
 # Limit the minimum upload size, which is only valid when downloading multiple BT files, and is used to filter useless files. Files below this size will be deleted and will not be uploaded.
-#MIN_SIZE=10m
+MIN_SIZE=100M
 
 # 保留文件类型，仅 BT 多文件下载时有效，用于过滤无用文件。其它文件将被删除，不会上传。
 # Keep the file type, only effective when downloading multiple BT files, used to filter useless files. Other files will be deleted and will not be uploaded.
@@ -27,7 +27,7 @@ export RCLONE_CONFIG=rclone.conf
 
 # RCLONE 块的大小，默认5M，理论上是越大上传速度越快，同时占用内存也越多。如果设置得太大，可能会导致进程中断。
 # RCLONE The size of the block, the default is 5M. Theoretically, the larger the upload speed, the faster it will occupy more memory. If the setting is too large, the process may be interrupted.
-export RCLONE_CACHE_CHUNK_SIZE=3M
+#export RCLONE_CACHE_CHUNK_SIZE=5M
 
 # RCLONE 块可以在本地磁盘上占用的总大小，默认10G。
 # RCLONE The total size that the block can occupy on the local disk, the default is 10G.
@@ -80,7 +80,7 @@ UPLOAD_FILE() {
     while [ ${RETRY} -le ${RETRY_NUM} ]; do
         [ ${RETRY} != 0 ] && (
             echo
-            echo -e "$(date +"%m/%d %H:%M:%S") ${ERROR} ${UPLOAD_PATH} Upload failed! Retry ${RETRY}/${RETRY_NUM} ..."
+            echo -e "$(date +"%m/%d %H:%M:%S") ${ERROR} Upload failed! Retry ${RETRY}/${RETRY_NUM} ..."
             echo
         )
         rclone copy -v "${UPLOAD_PATH}" "${REMOTE_PATH}"
